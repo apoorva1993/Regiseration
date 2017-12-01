ee35bc0da0c1f629de447c917081ec8051321a14
Apoorva Baranwal <Apoorva.Baranwal@HANDYGOIT.COM>
2017-12-01 12:57:54 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Regiseration
 Validate the registration page
+
+
+Requirement before running the automated test case
+(all the requirement is fulfil on windows 8)
+
+As we will run the automation test code, so for this we need to install “pip, Python version 3.x.x (as I wrote the code in Python to automate the registration form) on windows.
+After that install selenium using command -
+>> pip install –U selenium
+Using above command, selenium will be installed in your system (internet connection should be connected).
+In the automation code, we have to set the path of the “chrome driver” so you need to download the Chrome Driver and set the path. Now, run the program where it is saved.
+
+
+Note- You will have to run the file using python command through the command prompt or in the relevent desktop app.
---
+++ reg.py
@@ -0,0 +1,117 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+import re
+
+ema= "[^@]+@[^@]+\.[^@]+"
+mob= "^[0-9]{10}$"
+
+def validfields(fname, lname, uname, pwd, cpwd, email1, mobile):
+    email_validation = re.match(ema, email1)
+    mobile_validation = re.match(mob, mobile)
+    
+    if len(fname)<=1:
+	   # print ("First name should be greater than 2")
+	    return False, "First name should have atleast 2"
+    elif len(lname)<=1:
+	    # print ("Last name should be greater than 2")
+	    return False, "Last name should have atleast 2"
+    elif len(uname)<8:
+	    # print ("Username should be minimum 8")
+	    return False, "Username should be minimum 8"
+    elif len(pwd)<8:
+	    # print ("Password should be minimum 8")
+	    return False, "Password should be minimum 8"
+    elif (pwd!=cpwd):
+	    # print ("Both password is not same")
+	    return False, "Your confirm password is not matching with the entered password"
+    elif email_validation==None:
+	    # print ("Your email-id is not valid")
+	    return False, "Your email-id is not valid"
+    elif mobile_validation==None:
+	    # print ("Mobile number should be 1 digit")
+	    return False, "Mobile number should be 10 digit"
+    else:	
+	    return True, "No Error Found"
+
+#Multiple Entries
+entry = [
+{"fname":"", "lname":"", "uname":"", "pwd":"", "cpwd":"", "email1":"", "mobile": ""},
+{"fname":"T", "lname":"Test", "uname":"testtest", "pwd":"12345678", "cpwd":"12345678", "email1":"abcd@test.com", "mobile": "9876543210"},
+{"fname":"Test", "lname":"T", "uname":"testtest", "pwd":"12345678", "cpwd":"12345678", "email1":"abcd@test.com", "mobile": "9876543210"},
+{"fname":"Test", "lname":"Test", "uname":"testtes", "pwd":"12345678", "cpwd":"12345678", "email1":"abcd@test.com", "mobile": "9876543210"},
+{"fname":"Test", "lname":"Test", "uname":"testtest", "pwd":"1234567", "cpwd":"12345678", "email1":"abcd@test.com", "mobile": "9876543210"},
+{"fname":"Test", "lname":"Test", "uname":"testtest", "pwd":"12345678", "cpwd":"1234567", "email1":"abcd@test.com", "mobile": "9876543210"},
+{"fname":"Test", "lname":"Test", "uname":"testtest", "pwd":"123456783213", "cpwd":"123456790", "email1":"abcd@test.com", "mobile": "9876543210"},
+{"fname":"Test", "lname":"Test", "uname":"testtest", "pwd":"12345678", "cpwd":"123456790", "email1":"abcd@test.com", "mobile": "9876543210"},
+{"fname":"Test", "lname":"Test", "uname":"testtest", "pwd":"12345678", "cpwd":"12345678", "email1":"ab@c.et", "mobile": "9876543210"},
+{"fname":"Test", "lname":"Test", "uname":"testtest", "pwd":"12345678", "cpwd":"12345678", "email1":"abcdtest.com", "mobile": "9876543210"},
+{"fname":"Test", "lname":"Test", "uname":"testtest", "pwd":"12345678", "cpwd":"12345678", "email1":"abcd@test.com", "mobile": "9876543210"},
+{"fname":"Test", "lname":"Test", "uname":"testtest", "pwd":"12345678", "cpwd":"12345678", "email1":"abcd@test.com", "mobile": "987654321"}
+]
+
+for data in entry:
+	fname = data['fname']
+	lname = data['lname']
+	uname = data['uname']
+	pwd = data['pwd']
+	cpwd = data['cpwd']
+	email1 = data['email1']
+	mobile = data['mobile']
+
+    #Chrome driver path
+	driverpath= "C:/Users/apoorva.baranwal/Desktop/Py_program/drivers/chromedriver.exe"
+	driver =webdriver.Chrome(driverpath)
+
+	#Base URL link
+	driver.get("http://adjiva.com/qa-test/")
+
+	#Enter first name
+	felem = driver.find_element_by_name("first_name")
+	felem.send_keys(fname)
+
+	#Enter last name
+	lelem = driver.find_element_by_name("last_name")
+	lelem.send_keys(lname)
+
+	#Select Dropdown option in Department
+	driver.find_element_by_name("department").send_keys("Engineering")	
+
+	#Enter username
+	uelem = driver.find_element_by_name("user_name")
+	uelem.send_keys(uname)
+
+	#Enter password
+	pelem = driver.find_element_by_name("user_password")
+	pelem.send_keys(pwd)
+
+	#Enter confirm password
+	cpelem = driver.find_element_by_name("confirm_password")
+	cpelem.send_keys(cpwd)
+
+	#Enter email address
+	emelem = driver.find_element_by_name("email")
+	emelem.send_keys(email1)
+
+	#Enter contact number
+	celem = driver.find_element_by_name("contact_no")
+	celem.send_keys(mobile)
+
+	#time.sleep(2)
+
+	status, message = validfields(fname, lname, uname, pwd, cpwd, email1, mobile)
+	print (message)
+	if status == True:
+		#Press submit button
+		driver.find_element_by_class_name("btn").click()
+		driver.execute_script("window.alert('Acknowledgement email has been sent to your registered email')")
+	else:
+		#error message on submit button
+		driver.find_element_by_class_name("btn").click()
+		driver.execute_script("window.alert('%s')"%message)
+		time.sleep(3)
+	    
+	time.sleep(10)
+
+	#Close driver
+	driver.close()
